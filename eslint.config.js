8ed3da3c8183a2084ec0382440c167cf59bc1fca
Sylvester Keil <sylvester@keil.or.at>
2026-03-15 00:43:32 +0100

    Include coverage report in test resolution

--- eslint.config.js
+++ eslint.config.js
@@ -9,6 +9,7 @@ export default standard({
   {
     rules: {
       curly: [1, 'multi-or-nest'],
+      'no-var': 0,
       'prefer-const': 0,
       '@stylistic/comma-dangle': 0,
       '@stylistic/padded-blocks': 0
