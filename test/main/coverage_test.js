73b4d86eb9c227e21037e19a423413022829dfb6
Sylvester Keil <sylvester@keil.or.at>
2026-03-15 00:27:12 +0100

    Add coverage support

---
+++ test/main/coverage_test.js
@@ -0,0 +1,39 @@
+import assert from 'node:assert/strict'
+import { test } from 'node:test'
+import { runMain, runRenderer } from '../../lib/spark.js'
+import { F } from '../support.js'
+
+test('coverage from main and renderer combined', async () => {
+  let coverages = []
+
+  let opts = {
+    globPatterns: [F.test('chamber')],
+    coverage: true,
+  }
+
+  async function collect (stream) {
+    for await (let event of stream) {
+      if (event.type === 'test:coverage')
+        coverages.push(event.data)
+    }
+  }
+
+  await Promise.all([
+    collect(runMain(opts)),
+    collect(runRenderer(opts))
+  ])
+
+  assert.equal(coverages.length, 2)
+
+  let covered = new Set()
+
+  for (let cov of coverages) {
+    let file = cov.summary.files.find((f) => f.path === F.js('chamber'))
+    assert.ok(file, 'chamber.js should appear in coverage')
+    for (let fn of file.functions)
+      if (fn.count > 0) covered.add(fn.name)
+  }
+
+  assert.ok(covered.has('ionize'), 'ionize should be covered')
+  assert.ok(covered.has('detect'), 'detect should be covered')
+})
