98bf5ea93d82840646e5a2ea7d115326eba7de46
Sylvester Keil <sylvester@keil.or.at>
2026-03-15 11:13:19 +0100

    Initialize app paths

---
+++ test/main/init_test.js
@@ -0,0 +1,26 @@
+import assert from 'node:assert'
+import { tmpdir } from 'node:os'
+import { test } from 'node:test'
+import { app } from 'electron'
+
+test('app name', () => {
+  assert.equal(app.getName(), 'spark')
+})
+
+test('app paths are in tmp dir', () => {
+  let tmp = tmpdir()
+
+  for (let name of [
+    'appData',
+    'userData',
+    'sessionData',
+    'cache',
+    'logs',
+    'crashDumps'
+  ]) {
+    assert(
+      app.getPath(name).startsWith(tmp),
+      `${name} should be in tmp`
+    )
+  }
+})
