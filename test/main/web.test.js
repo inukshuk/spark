b453d57e979867d255db748988db638e5253dc5d
Sylvester Keil <sylvester@keil.or.at>
2026-03-20 09:04:54 +0100

    Refactor runRenderer

--- test/main/web.test.js
+++ test/main/web.test.js
@@ -4,11 +4,17 @@ import { F } from '../support/fixtures.js'
 import { assertTestNames } from '../support/stream.js'
 
 let files = [F.test('web')]
+let WebAPIs = [
+  'indexedDB',
+  'localStorage',
+  'sessionStorage'
+]
 
 test('web APIs are available by default', () =>
-  assertTestNames(runRenderer({ files }), ['indexedDB', 'localStorage', 'sessionStorage']))
+  assertTestNames(runRenderer({ files }), WebAPIs))
 
 test('web APIs are not available with about:blank', () =>
-  assertTestNames(runRenderer({
-    files, url: 'about:blank'
-  }), ['indexedDB', 'localStorage', 'sessionStorage'], 'fail'))
+  assertTestNames(
+    runRenderer({ files, url: 'about:blank' }),
+    WebAPIs,
+    'fail'))
